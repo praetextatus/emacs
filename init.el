c1ae0c5b847949af0ad871e085fcc9c6e8dbf0bf
peter <pmostowsky@gmail.com>
2016-04-09 01:02:57 +0300

    Font settings

--- init.el
+++ init.el
@@ -4,15 +4,20 @@
 (tool-bar-mode 0)
 (set-scroll-bar-mode 'right)
 
-;;linum mode and highliting of current line
+;; Better looks
 (global-linum-mode 1)
 (global-hl-line-mode 1)
+(show-paren-mode 1)
 
-;;printing settings
+;; fonts
+(add-to-list 'default-frame-alist '(font . "Droid Sans Mono-12"))
+
+
+;; printing settings
 (setq printer-name "Deskjet-F2200-series")
 (global-set-key [?\C-c ?\C-p] 'print-buffer)
 
-;;coding style
+;; coding style
 (setq-default c-default-style "ellemtel"
       c-basic-offset 4
       tab-width 4
@@ -20,21 +25,21 @@
 (defvaralias 'c-basic-offset 'tab-width)
 
 
-;;hook for c++ mode
+;; hook for c++ mode
 (defun my-c++-mode-hook ()
   (define-key c++-mode-map [?\C-c ?\C-c] 'compile)
   (define-key c++-mode-map [?\C-c d]   'gdb)
   (c-set-offset 'access-label '0)
   (c-set-offset 'inclass '+)
-  (linum-mode))
+  (auto-complete-mode))
 (add-hook 'c++-mode-hook 'my-c++-mode-hook)
 
 
-;;treat .h files as c++ 
+;; treat .h files as c++ 
 (add-to-list 'auto-mode-alist '("\\.h\\'" . c++-mode))
 
 
-;;special keys
+;; special keys
 (global-set-key "\C-cl" 'goto-line)
 
 (when (>= emacs-major-version 24)
@@ -43,8 +48,24 @@
   (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t))
 
 
+;; color theme -- tango
+(when (>= emacs-major-version 24)
+  (load-theme 'tango t))
 
 
 
 
 
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(inhibit-startup-screen t)
+ '(safe-local-variable-values (quote ((tab-indent-mode . t)))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
